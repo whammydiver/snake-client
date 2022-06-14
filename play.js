0d9cb65c33c19618a27ea6712e4a8c50c1f51d22
Paul Taylor <paul@whammydiver.com>
2022-06-13 18:35:23 -0600

    connect and recieve message

--- play.js
+++ play.js
@@ -1,1 +1,23 @@
-console.log('testfile');
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+    // client.js
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
