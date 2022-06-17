4135483ae972efbc80a68ef78d2d2ce11c03e08a
Paul Taylor <paul@whammydiver.com>
2022-06-16 22:23:34 -0500

    IT LIVES!!!!

--- input.js
+++ input.js
@@ -1,15 +1,32 @@
+const { connect } = require("./client");
+
+let connection;
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up');
+  } 
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
 };
 
-const setupInput = function () {
+const setupInput = (conn) => {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on("data", handleUserInput);
+  connection = conn;
   return stdin;
 };
 
--- play.js
+++ play.js
@@ -2,7 +2,7 @@ const { connect } = require("./client");
 const { setupInput } = require('./input');
 
 console.log('Connecting...');
-connect();
+let move = connect();
 
-setupInput();
+setupInput(move);
 
