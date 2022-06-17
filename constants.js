b1f2761b1c007adca9b9ec9904984cd37ec516bf
Paul Taylor <paul@whammydiver.com>
2022-06-16 22:52:21 -0500

    constraints and DRY key and smacktalk assignment

---
+++ constants.js
@@ -0,0 +1,15 @@
+const IP = 'localhost';
+const PORT = 50541;
+const movesAndSmackTalk = {
+  w: 'Move: up',
+  s: 'Move: down',
+  a: 'Move: left',
+  d: 'Move: right',
+  1: 'Say: Eat my shorts',
+  2: 'Say: Slither on, yo.',
+  3: 'Say: Snake or snail?',
+  4: 'Say: Speed Demon 2000!'
+}
+
+
+module.exports = { IP, PORT, movesAndSmackTalk };
--- input.js
+++ input.js
@@ -1,4 +1,5 @@
 const { connect } = require("./client");
+const { movesAndSmackTalk } = require("./constants");
 
 let connection;
 
@@ -6,30 +7,11 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
-    connection.write('Move: up');
-  } 
-  if (key === 's') {
-    connection.write('Move: down');
+
+  if (key in movesAndSmackTalk) {
+    connection.write(`${movesAndSmackTalk[key]}`);
   }
-  if (key === 'a') {
-    connection.write('Move: left');
-  }
-  if (key === 'd') {
-    connection.write('Move: right');
-  }
-  if (key === '1') {
-    connection.write('Say: Eat my shorts');
-  }
-  if (key === '2') {
-    connection.write('Say: Slither on, yo.');
-  }
-  if (key === '3') {
-    connection.write('Say: Snake or snail?');
-  }
-  if (key === '4') {
-    connection.write('Say: Speed Demon 2000!');
-  }
+
 };
 
 const setupInput = (conn) => {
